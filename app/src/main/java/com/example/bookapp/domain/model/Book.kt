8aac68ab7534f80d918778c92060f950e79e6cac
Dario DAngelo <dariodangelo21@gmail.com>
2022-12-30 17:13:35 -0300

    ApiResponse and BookApi Ready

--- app/src/main/java/com/example/bookapp/domain/model/Book.kt
+++ app/src/main/java/com/example/bookapp/domain/model/Book.kt
@@ -3,7 +3,9 @@ package com.example.bookapp.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.bookapp.util.Constants.BOOK_DATABASE_TABLE
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = BOOK_DATABASE_TABLE)
 data class Book(
     @PrimaryKey(autoGenerate = false)
