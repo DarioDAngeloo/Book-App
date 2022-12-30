0be4d6d74874e98f557257bd6189fd33030df94a
Dario DAngelo <dariodangelo21@gmail.com>
2022-12-30 20:18:16 -0300

    Implement RemoteMediator and load() function - Final Version

--- app/src/main/java/com/example/bookapp/domain/model/BookRemoteKeys.kt
+++ app/src/main/java/com/example/bookapp/domain/model/BookRemoteKeys.kt
@@ -2,10 +2,10 @@ package com.example.bookapp.domain.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.bookapp.util.Constants.BOOK_REMOTE_KEY_DATABASE_TABLE
+import com.example.bookapp.util.Constants.BOOK_REMOTE_KEYS_DATABASE_TABLE
 
-@Entity(tableName = BOOK_REMOTE_KEY_DATABASE_TABLE)
-data class BookRemoteKey(
+@Entity(tableName = BOOK_REMOTE_KEYS_DATABASE_TABLE)
+data class BookRemoteKeys(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
     val prevPage: Int?,
