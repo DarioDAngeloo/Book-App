0ccdc35bea2614db4591e7270a60b1b9358f4e7b
Dario DAngelo <dariodangelo21@gmail.com>
2022-12-31 11:44:19 -0300

    Stars components Calculation Ready

--- app/src/main/java/com/example/bookapp/presentation/components/RatingWidget.kt
+++ app/src/main/java/com/example/bookapp/presentation/components/RatingWidget.kt
@@ -1,10 +1,15 @@
 package com.example.bookapp.presentation.components
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Path
@@ -14,17 +19,23 @@ import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.vector.PathParser
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.bookapp.R
 import com.example.bookapp.ui.theme.LightGray
+import com.example.bookapp.ui.theme.SMALL_EXTRA_PADDING
 import com.example.bookapp.ui.theme.starColor
 
 @Composable
 fun RatingWidget(
     modifier: Modifier,
     rating: Double,
-    scaleFactor: Float = 1.5f
+    scaleFactor: Float = 1.5f,
+    spaceBetween: Dp = SMALL_EXTRA_PADDING
 ) {
+    val result = calculateStars(rating = rating)
+
+
     val starPathString = stringResource(id = R.string.star_path)
     val starPath = remember {
         PathParser().parsePathString(pathData = starPathString).toPath()
@@ -33,7 +44,38 @@ fun RatingWidget(
         starPath.getBounds()
     }
 
-    FilledStar(starPath = starPath, starPathBounds = starPathBounds, scaleFactor = 1.5f)
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(SMALL_EXTRA_PADDING)
+    ) {
+        result["filledStars"]?.let {
+            repeat(it){
+                FilledStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+        result["halfFilledStars"]?.let {
+            repeat(it){
+                HalfFilledStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+        result["emptyStars"]?.let {
+            repeat(it){
+                EmptyStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+    }
 }
 
 @Composable
@@ -129,6 +171,45 @@ fun EmptyStar(
 
         }
     }
+
+}
+
+
+@Composable
+fun calculateStars(rating: Double): Map<String, Int> {
+    val maxStar by remember { mutableStateOf(5) }
+    var filledStars by remember { mutableStateOf(0) }
+    var halfFilledStars by remember { mutableStateOf(0) }
+    var emptyStars by remember { mutableStateOf(0) }
+
+    LaunchedEffect(key1 = rating) {
+        val (firstNumber, lastNumber) = rating.toString()
+            .split(".")
+            .map { it.toInt() }
+
+        if (firstNumber in 0..5 && lastNumber in 0..9) {
+            filledStars = firstNumber
+            if (lastNumber in 1..5) {
+                halfFilledStars++
+            }
+            if (lastNumber in 6..9) {
+                filledStars++
+            }
+            if (firstNumber == 5 && lastNumber > 0) {
+                emptyStars = 5
+                filledStars = 0
+                halfFilledStars = 0
+            }
+        } else {
+            Log.d("RatingWidget", "invalid rating number")
+        }
+    }
+    emptyStars = maxStar - (filledStars + halfFilledStars)
+    return mapOf(
+        "filledStars" to filledStars,
+        "halfFilledStars" to halfFilledStars,
+        "emptyStars" to emptyStars
+    )
 
 }
 
--- app/src/main/java/com/example/bookapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/bookapp/presentation/screens/home/HomeScreen.kt
@@ -1,9 +1,13 @@
 package com.example.bookapp.presentation.screens.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.bookapp.presentation.components.RatingWidget
+import com.example.bookapp.ui.theme.LARGE_PADDING
 
 @Composable
 fun HomeScreen(
@@ -12,7 +16,7 @@ fun HomeScreen(
 
     val allBooks = homeViewModel.getAllBooks.collectAsLazyPagingItems()
 
-    Scaffold(topBar = { HomeTopBar({})}) {
-
+    Scaffold(topBar = { HomeTopBar(onSearchClicked = {}) }) {
+        RatingWidget(modifier = Modifier.padding(all = LARGE_PADDING), rating = 1.9)
     }
 }
