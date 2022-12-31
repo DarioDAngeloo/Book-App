8a50e5f276133a3c20e5b11417e46c8288c9ad5e
Dario DAngelo <dariodangelo21@gmail.com>
2022-12-31 14:59:20 -0300

    Book Item Ready and dependecy updates

--- app/src/main/java/com/example/bookapp/navigation/Screen.kt
+++ app/src/main/java/com/example/bookapp/navigation/Screen.kt
@@ -4,9 +4,9 @@ sealed class Screen(val route: String) {
     object Splash : Screen("splash_screen")
     object Welcome : Screen("welcome_screen")
     object Home : Screen("home_screen")
-    object Details : Screen("details_screen/{heroId}") {
-        fun passHeroId(heroId: Int): String {
-            return "details_screen/$heroId"
+    object Details : Screen("details_screen/{bookId}") {
+        fun passBookId(bookId: Int): String {
+            return "details_screen/$bookId"
         }
     }
 
