d922f5c4cd71e17dc25b00352994081afd9086c5
Dario DAngelo <dariodangelo21@gmail.com>
2023-01-02 13:07:50 -0300

    Search Screen - Implement Search Books UseCase

--- app/src/main/java/com/example/bookapp/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/bookapp/presentation/screens/search/SearchScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.paging.compose.collectAsLazyPagingItems
 
 @Composable
 fun SearchScreen(
@@ -13,6 +14,7 @@ fun SearchScreen(
 ) {
 
     val searchQuery by searchViewModel.searchQuery
+    val books = searchViewModel.searchedBooks.collectAsLazyPagingItems()
 
     Scaffold(
         topBar = {
@@ -21,7 +23,9 @@ fun SearchScreen(
                 onTextChange = {
                                searchViewModel.updateSearchQuery(query = it)
                 },
-                onSearchClicked = {},
+                onSearchClicked = {
+                                  searchViewModel.searchBooks(query = it)
+                },
                 onCloseClicked = {
                     navHostController.popBackStack()
                 }
