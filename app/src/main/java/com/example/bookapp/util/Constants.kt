cafeb98aaf8c1379118e9e7973e38639cc11ce19
Dario DAngelo <dariodangelo21@gmail.com>
2023-01-05 00:57:56 -0300

    FINAL DETAIL ALL WORKING AND THEME WORKING (ALL)

--- app/src/main/java/com/example/bookapp/util/Constants.kt
+++ app/src/main/java/com/example/bookapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.bookapp.util
 
 object Constants {
 
-    const val BASE_URL = "http://10.0.2.2:8080"
+    const val BASE_URL = "https://fine-o4pw.onrender.com/"
 
     const val DETAILS_ARGUMENT_KEY = "bookId"
 
